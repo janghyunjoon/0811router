790819170cbd8e750236a8e9c7979061a3927cb5
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-12 09:13:15 +0900

    .

---
+++ src/pages/BoardDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+const BoardDetail = () => {
+    const {id}=useParams()
+    const nav = useNavigate()
+    return (
+        <section>
+            <h1>게시글 상세</h1>
+            <p>현재 글 ID : </p>
+            <button onClick={()=>nav(-1)}>뒤로가기</button>
+            <button onClick={()=>nav('/')}>홈으로 가기</button>
+        </section>
+    )
+}
+
+export default BoardDetail
