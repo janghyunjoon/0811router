7741d8806edc26d487bd87a3230d43944c66ffac
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-27 10:46:15 +0900

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import HomeHero from "./home/HomeHero"
+import HomeHero from './home/HomeHero'
 
 const Home = () => {
-  return (
-    <section>
-<HomeHero/>
-    </section>
-  )
+    return (
+        <section>
+            <h1>
+            <HomeHero/>
+            </h1>
+            </section>
+    )
 }
 
 export default Home
